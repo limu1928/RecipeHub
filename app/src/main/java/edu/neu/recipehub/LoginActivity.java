f74fd1644a30457ffb51e8a3985a8097689637d8
Junda <yang.jund@husky.neu.edu>
2018-11-26 16:05:52 -0800

    initialization

---
+++ app/src/main/java/edu/neu/recipehub/LoginActivity.java
@@ -0,0 +1,44 @@
+package edu.neu.recipehub;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import edu.neu.recipehub.users.UserEntry;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    /**
+     * Start activity with customized user name.
+     * @param view
+     */
+    public void startButtonOnClick(View view){
+        EditText editText = findViewById(R.id.loginNameEditText);
+        String userName = editText.getText().toString();
+        Intent intent = new Intent(this,HomeActivity.class);
+
+        intent.putExtra(UserEntry.USER_NAME,userName);
+
+        startActivity(intent);
+    }
+
+    /**
+     * Start activity by default user name.
+     * @param view
+     */
+    public void startAsAnonymousButtonOnClick(View view){
+        Intent intent = new Intent(this,HomeActivity.class);
+
+        intent.putExtra(UserEntry.USER_NAME,UserEntry.ANONYMOUS_USER);
+
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/edu/neu/recipehub/users/UserEntry.java
@@ -0,0 +1,13 @@
+package edu.neu.recipehub.users;
+
+
+/**
+ * A class recording the corresponding key and default values for user information.
+ */
+public class UserEntry {
+
+    public static final String USER_NAME = "user name";
+
+    public static final String ANONYMOUS_USER = "Anonymous";
+
+}
